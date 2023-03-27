f5cfed710a6659b69324efd24d2e8751d080936d
Mohamed Duse <86656964+90duse@users.noreply.github.com>
2023-03-27 14:18:32 +0300

    Add files via upload

---
+++ pythonchallengesSolved/solvedchallenge1.py
@@ -0,0 +1,18 @@
+#Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them
+# name = input("what is your first name")
+# lastname= input("What is your last name")
+# print("Nice to meet you", lastname, name)
+
+
+#Write a Python program that calculates the area of a circle based on the radius entered by the user.
+# PI = 3.14 
+# raduis = float(input("Enter radius"))
+# A = PI * raduis*raduis
+# print("erea of circle is: ", A)
+
+
+import random 
+
+for duse in range(10):
+    x = random.random()
+    print(x)
---
+++ pythonchallengesSolved/solvedchallenge2.py
@@ -0,0 +1,5 @@
+# Write a Python program that calculates the area of a circle based on the radius entered by the user.
+PI = 3.14 
+raduis = float(input("Enter radius"))
+A = PI * raduis*raduis
+print("erea of circle is: ", A)
